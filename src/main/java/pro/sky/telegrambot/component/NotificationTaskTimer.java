9a40baca5ff64cb6b118c8c3b2d469acc2352394
eremi <ereminr83@mail.ru>
2023-10-09 22:16:47 +0700

    2 commit

--- src/main/java/pro/sky/telegrambot/component/NotificationTaskTimer.java
+++
@@ -1,4 +0,0 @@
-package pro.sky.telegrambot.component;
-
-public class NotificationTaskTimer {
-}
---
+++ src/main/resources/liquibase/scripts/notification.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset roman:1
+CREATE TABLE notification_task (
+
+    id bigserial primary key,
+    chat_id bigint,
+    text varchar,
+    exec_date timestamp
+    )
